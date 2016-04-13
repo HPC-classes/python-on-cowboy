adde6484090ca9c94353dd2bc7b45aa4ae32ece8
dbrunson <dana.s.brunson@gmail.com>
2016-04-13 14:47:18 -0500

    alldata.py addition

---
+++ alldata.py
@@ -0,0 +1,51 @@
+import numpy
+import glob
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot
+
+# % matplotlib inline
+data = numpy.loadtxt(fname='data/inflammation-01.csv', delimiter=',')
+alldatamean = data.mean(axis=0)
+alldatamax = data.max(axis=0)
+alldatamin = data.min(axis=0)
+
+filenames = glob.glob('data/inflammation*.csv')
+filenames = filenames[1:]
+for f in filenames:
+    #print(f)
+    data = numpy.loadtxt(fname=f, delimiter=',')
+    # print("mean is", data.mean(axis=0))
+    #alldatamean = data.mean(axis=0)
+    #print("max is", data.max(axis=0))
+    #print("min is", data.min(axis=0))
+    alldatamean = alldatamean + data.mean(axis=0)
+    alldatamax = numpy.maximum(alldatamax, data.max(axis=0))
+    alldatamin = numpy.minimum(alldatamin, data.min(axis=0))
+print('all data mean is:', alldatamean/12)
+print('all data max is:', alldatamax)
+print('all data min is:', alldatamin)
+
+fig = matplotlib.pyplot.figure(figsize=(10.0 ,3.0))
+
+fig.suptitle('all the data')
+
+axes1 = fig.add_subplot(1, 3, 1)
+axes2 = fig.add_subplot(1, 3, 2)
+axes3 = fig.add_subplot(1, 3, 3)
+
+axes1.set_ylabel('average')
+axes1.plot(alldatamean/12)
+
+axes2.set_ylabel('max')
+axes2.plot(alldatamax)
+
+axes3.set_ylabel('min')
+axes3.plot(alldatamin)
+
+fig.tight_layout()
+
+# matplotlib.pyplot.show()
+
+matplotlib.pyplot.savefig('alldata.pdf')
+
