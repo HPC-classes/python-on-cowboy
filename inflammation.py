df6fbffa782336c6073e27a2cb509c9912e4f283
dbrunson <dana.s.brunson@gmail.com>
2016-04-13 11:58:09 -0500

    nonworking average thingy

--- inflammation.py
+++ inflammation.py
@@ -5,10 +5,13 @@ import glob
 
 filenames = glob.glob('data/inflammation*.csv')
 filenames = filenames[0:3]
+alldatamean = []
 for f in filenames:
     print(f)
     data = numpy.loadtxt(fname=f, delimiter=',')
-    print("mean is",data.mean(axis=0))
-    print(data.max(axis=0))
-    print(data.min(axis=0))
-    
+    print("mean is", data.mean(axis=0))
+    #print("max is", data.max(axis=0))
+    #print("min is", data.min(axis=0))
+    alldatamean = numpy.vstack([alldatamean,data.mean(axis=0)])
+    print(alldatamean)
+
