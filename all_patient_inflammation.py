21c1772736f09539132df045e920055dacb9b882
Phillip Doehle <doehle@okstate.edu>
2016-04-22 14:47:52 -0500

    Script prints out to .csv file.

--- all_patient_inflammation.py
+++ all_patient_inflammation.py
@@ -2,41 +2,46 @@
 # across all patients 
 import numpy
 import glob
+import os
 
-#get file names for all the data files
+# setup directory for .csv output
+if not os.path.exists('inflammation_output'):
+    os.makedirs('inflammation_output')
+
+# get file names for all the data files
 filenames = glob.glob('data/inflammation*.csv')
 
-#setup lists for holding accumulated data
+# setup lists for holding accumulated data
 alldatamean = list()
 alldatamax = list()
 alldatamin = list()
 
 for f in filenames:
-    #import data and show status
+    # import data and show status
     print(f)
     data = numpy.loadtxt(fname=f, delimiter=',')
     
-    #convert data from arrays to lists so can append later
+    # convert data from arrays to lists so can append later
     meanAsList = data.mean(axis=0).tolist()
     maxAsList = data.max(axis=0).tolist()
     minAsList = data.min(axis=0).tolist()
     
-    #print data for each specific file
+    # print data for each specific file to standar output
     print("mean is:",'\n--------\n', meanAsList)
     print("\nmax is:",'\n-------\n', maxAsList)
     print("\nmin is:",'\n-------\n', minAsList, '\n\n')
     
-    #append this file's data to the overall data
+    # append this file's data to the overall data
     alldatamean.append(meanAsList)
     alldatamax.append(maxAsList)
     alldatamin.append(minAsList)
     
-#change back into array in order to enjoy numpy goodness
+# change back into array in order to enjoy numpy goodness
 alldatamean = numpy.array(alldatamean)
 alldatamax = numpy.array(alldatamax)
 alldatamin = numpy.array(alldatamin)
 
-#output overall data
+# output overall data to standard output
 print("\n\n\n\n*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*\n")
 print("Overall Data by Day:\n\n")
 
@@ -48,6 +53,20 @@ number of patients is unkown for any given file then the
 code must be changed to accommodate for a weighted average!
 '''
 
+# output mean, min, and max
 print("mean is:",'\n--------\n', alldatamean.mean(axis=0))
 print("\nmax is:",'\n-------\n', alldatamax.max(axis=0))
 print("\nmin is:",'\n-------\n', alldatamin.min(axis=0), '\n\n')
+
+''' 
+append mean, min, and max to last row of array so the whole
+thing can be written out to a .csv file
+'''
+alldatamean = numpy.vstack([alldatamean, alldatamean.mean(axis=0)])
+alldatamax = numpy.vstack([alldatamax, alldatamax.max(axis=0)])
+alldatamin = numpy.vstack([alldatamin, alldatamin.min(axis=0)])
+
+# output results to csv
+numpy.savetxt('inflammation_output/mean.csv', alldatamean, delimiter=',')
+numpy.savetxt('inflammation_output/max.csv', alldatamax, delimiter=',')
+numpy.savetxt('inflammation_output/min.csv', alldatamin, delimiter=',')
